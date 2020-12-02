e041cc589d571d71fc05a608a27ae2ade01c906b
Peter Mezei <mezeipetister@gmail.com>
2020-12-02 16:50:39 +0100

    basic structure and logic

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use chrono::prelude::*;
 use packman::*;
 use serde::{Deserialize, Serialize};
 use std::ops::Mul;
+use std::path::PathBuf;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum VAT {
@@ -61,15 +62,48 @@ pub struct HistoryItem {
     created_at: DateTime<Utc>,
 }
 
+impl Default for HistoryItem {
+    fn default() -> Self {
+        Self {
+            net_retail_price: 0,
+            vat: VAT::default(),
+            gross_retail_price: 0,
+            created_by: "".into(),
+            created_at: Utc::now(),
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Sku {
     sku: u32,
     net_retail_price: u32,
     vat: VAT,
     gross_retail_price: u32,
-    history: Vec<()>,
+    history: Vec<HistoryItem>,
+}
+
+impl Default for Sku {
+    fn default() -> Self {
+        Self {
+            sku: 0,
+            net_retail_price: 0,
+            vat: VAT::default(),
+            gross_retail_price: 0,
+            history: Vec::new(),
+        }
+    }
+}
+
+impl VecPackMember for Sku {
+    type Out = u32;
+
+    fn get_id(&self) -> &Self::Out {
+        &self.sku
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let db: VecPack<Sku> = VecPack::load_or_init(PathBuf::from("data/pricing"))
+        .expect("Error while loading pricing db");
 }
